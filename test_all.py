5fd1628b104207bf1a8e96baf48b174787b1c856
Antoine Boizard <boizard@apc.in2p3.fr>
2017-05-31 13:59:20 +0200

    partioned synthax error fix

--- test_all.py
+++ test_all.py
@@ -9,4 +9,4 @@ def test_division():
 def test_numpy_partition():
     input_arr = np.array([3, 2, -1, 10])
     partitioned = np.partition(input_arr,1)
-    assert partitionded[1] == 2
+    assert partitioned[1] == 2
