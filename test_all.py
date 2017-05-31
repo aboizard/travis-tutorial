15b07684c5d6e3ae0a226485f0b29a6ed6a480d3
aboizard <boizard@apc.in2p3.fr>
2017-05-31 12:22:10 +0200

    Add Test_numpy

--- test_all.py
+++ test_all.py
@@ -3,3 +3,8 @@ def test_addition():
 
 def test_division():
     assert 3./2 == 1.5
+
+def test_numpy_partition():
+    input_arr = np.array([3, 2, -1, 10])
+    partitioned = np.partition(input_arr,1)
+    assert partitionded[1] == 2
